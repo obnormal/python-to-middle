084cc9df4da6484c80bbf6c5004d9ad68f6d1bd3
Kamil Gabdullin <kamilgabdullin@gmail.com>
2022-06-11 17:51:18 +0300

    block_1 Управление памятью

--- block_1/memory_usage/task_1/implementation.py
+++ block_1/memory_usage/task_1/implementation.py
@@ -1,5 +1,15 @@
+import os
+import weakref
+
+
 class TempFile:
 
     def __init__(self, path) -> None:
         super().__init__()
         self._f = open(path, 'wb')
+        self._finalizer = weakref.finalize(self, self.on_delete, self._f, path)
+
+    @staticmethod
+    def on_delete(file, path):
+        file.close()
+        os.remove(path)
