b4bcf18dd3b0a7515eddef4b36c65abfcf218c41
Kamil Gabdullin <kamilgabdullin@gmail.com>
2022-06-20 19:50:15 +0300

    block_2 Наследование и Метаклассы commit2

--- block_2/metaclasses/task_2/implementation.py
+++ block_2/metaclasses/task_2/implementation.py
@@ -1,4 +1,44 @@
+
+
+class LazyMeta(type):
+    @classmethod
+    def __prepare__(mcs, name, bases):
+        a = 1
+        return {}
+
+    def __call__(cls, expression, *args, **kwargs):
+        expression_result = expression()
+
+        # Пересоздадим класс при вызове отнаследуя от результата expression
+        cls_instance = LazyMeta.__new__(
+            LazyMeta,
+            cls.__name__,
+            (type(expression_result),),
+            {expression.__name__: expression},
+
+        )
+
+        # Активируем базовый call
+        instance = super().__call__(cls_instance, expression, expression_result, *args, **kwargs)
+
+        return instance
+
+    def __new__(mcs, cls_name, bases, classdict):
+        cls_instance = super().__new__(mcs, cls_name, bases, classdict)
+
+        return cls_instance
+
+
 class Lazy(metaclass=LazyMeta):
-    def __init__(self, expression) -> None:
-        super().__init__()
+
+    def __new__(cls, cls_instance, expression, expression_result, *args):
+        res = cls_instance.__new__(
+            cls_instance,
+            expression_result,
+            *args,
+        )
+        return res
+
+    def __init__(self, expression, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         object.__setattr__(self, 'expression', expression)
