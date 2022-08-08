62453b667b170f2b6e1ed02449e3b6fdd3804b48
Gabdullin Kamil <kamilgabdullin@gmail.com>
2022-08-08 04:52:30 +0300

    block_8 acid, locks, mvcc

--- block_8/locks/task_1/models.py
+++ block_8/locks/task_1/models.py
@@ -6,11 +6,17 @@ from django.db import (
 class Account(models.Model):
 
     balance = models.IntegerField(default=0)
+    version = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'locks_1_account'
 
     def deposit(self, amount):
+        actual_account = self._get_actual_account()
+        if self.version != actual_account['version']:
+            self.__dict__.update(actual_account)
+
+        self.version += 1
         self.balance += amount
         self.save()
 
@@ -18,5 +24,13 @@ class Account(models.Model):
         if amount > self.balance:
             raise ValueError('Сумма снятия больше, чем баланс счета')
 
+        actual_account = self._get_actual_account()
+        if self.version != actual_account['version']:
+            self.__dict__.update(actual_account)
+
+        self.version += 1
         self.balance -= amount
         self.save()
+
+    def _get_actual_account(self):
+        return self.__class__.objects.filter(pk=self.pk).values('balance','version').first()
---
+++ block_8/locks/task_2/exceptions.py
@@ -0,0 +1,2 @@
+class WrongBalanceVersion(Exception):
+    """Неверная версия баланса"""
